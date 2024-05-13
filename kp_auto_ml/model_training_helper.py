55ee8618de315afc85cce0a40e0f0a9629ee5ddb
KinderPreet Singh <kinderpreet21@outlook.com>
2024-05-13 18:21:39 +0530

    Model training with models automated. Performance dataframe with score and RMSE.

--- kp_auto_ml/model_training_helper.py
+++ kp_auto_ml/model_training_helper.py
@@ -1,10 +1,18 @@
+from kp_auto_ml import model_training_data_prep as dp
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import Ridge
 from kp_auto_ml import model_training_data_prep as dp
+from kp_auto_ml import model_training_helper as mth
 
 from enum import Enum
+import pandas as pd
+from sklearn.metrics import mean_squared_error
+import warnings
 
+# Filter out specific warning messages
+warnings.filterwarnings('ignore', category=DeprecationWarning)
+warnings.filterwarnings('ignore', category=FutureWarning)
 
 class ModelPower(Enum):
     LITE = 'li'
@@ -52,19 +60,22 @@ def get_model_and_param(power:ModelPower,model_and_param:ModelAndParam):
     model = None
     param = None
     if model_and_param == ModelAndParam.Linear_Regression:
-        model,param = get_parameters_linear_reg()
+        param,model = get_parameters_linear_reg()
     elif model_and_param == ModelAndParam.Ridge_Regression:
-        model,param = get_parameters_ridge_fit(power=power)
-
-    # if model & param is None:
-    #     raise ValueError("Invalid model_and_param type.")
+        param,model = get_parameters_ridge_fit(power=power)
 
     return model,param
 
 
 
-def train_test_random_search_regression(model, param_distributions, X_train, y_train, X_test, y_test, n_iter=10, scoring='r2', cv=5):
-    random_search = RandomizedSearchCV(estimator=model, param_distributions=param_distributions, n_iter=n_iter, scoring=scoring, cv=cv, random_state=42)
+def train_test_random_search_regression(model, param_distributions, X_train, y_train, X_test, y_test, scoring='r2', cv=5):
+    total_combinations = 1
+    for values in param_distributions.values():
+        total_combinations *= len(values)
+    # Set n_iter based on the total_combinations
+    n_iter = min(total_combinations, 10)
+
+    random_search = RandomizedSearchCV(estimator=model, param_distributions=param_distributions,n_iter=n_iter, scoring=scoring, cv=cv, random_state=42)
     random_search.fit(X_train, y_train)
     
     best_params = random_search.best_params_
@@ -75,3 +86,51 @@ def train_test_random_search_regression(model, param_distributions, X_train, y_t
     test_score = best_model.score(X_test, y_test)
     
     return best_params, best_model, test_score
+
+
+class ModelPerformance():
+    score = None
+    model_name = None
+    RMSE = None
+    def __init__(self,score,model_name,RMSE = None) -> None:
+        self.model_name = model_name
+        self.score = score
+        self.RMSE = RMSE
+
+    def to_dict(self):
+        return vars(self)
+        
+def insert_object_columns(df, obj:ModelPerformance):
+    if df is None:
+        df = pd.DataFrame(columns=obj.to_dict())
+    
+    row_data = obj.to_dict()
+    
+    df_new = pd.concat([df, pd.DataFrame([row_data])], ignore_index=True)
+    
+    return df_new
+
+from sklearn.metrics import mean_squared_error as _mean_squared_error
+from sklearn.metrics import root_mean_squared_error as _root_mean_squared_error
+
+
+
+class ModelTrainer():
+    performance_df:pd.DataFrame = None
+    data:dp.ModelTrainingData = None
+    def __init__(self,data:dp.ModelTrainingData) -> None:
+        self.data = data
+
+    def perform_operation(self):
+        for scaler in ModelAndParam:
+            model,param =get_model_and_param(power=ModelPower.HIGH,model_and_param=scaler)
+            for X_train, X_val, X_test in self.data.generate_permutations_train(min_columns=len(self.data.X_original.columns)-1):
+                best_param,best_model,score = train_test_random_search_regression(model=model,param_distributions=param,X_train=X_train,y_train=self.data.Y_train,X_test=X_val,y_test=self.data.Y_val)
+                
+                y_pred = best_model.predict(X_test)
+                from sklearn.metrics import mean_squared_error
+
+                rmse = mean_squared_error(self.data.Y_test, y_pred, squared=False)
+                model_performance = ModelPerformance(score=score,model_name=scaler.name,RMSE=rmse)
+                self.performance_df = insert_object_columns(self.performance_df,model_performance)
+    
